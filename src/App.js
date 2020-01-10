27fb7eb19dc186f6aec3881ac586ba921d76617b
Raul Gomez <raul_nouni@hotmail.com>
2020-01-10 15:54:08 -0600

    Create a custom hook to manage the logic of the game

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
+import useGame from './hooks/useGame'
 
 const App = () => {
-  const TIME = 5
-  const [text, setText] = useState('')
-  const [timeRemaining, setTimeRemaining] = useState(TIME)
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
-  const [wordCount, setWordCount] = useState(0)
-  const textBoxRef = useRef(null)
-
-  const handleChange = (event) => {
-    const { value } = event.target
-    setText(value)
-  }
-
-  const calculateWords = (str) => {
-    const wordsArray =  str.trim().split(' ')
-    return wordsArray.filter(word => word !== '').length
-  }
-
-  const startGame = () => {
-    setIsTimeRunning(true)
-    setTimeRemaining(TIME)
-    setText('')
-    textBoxRef.current.disabled = false
-    textBoxRef.current.focus()
-  }
-
-  const endGame = () => {
-    setIsTimeRunning(false)
-    setWordCount(calculateWords(text))
-  }
-
-  useEffect(() => {
-    if(isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => {
-          setTimeRemaining(time => time - 1)
-      }, 1000)
-    } else if(timeRemaining === 0) {
-      endGame()
-    }
-  }, [timeRemaining, isTimeRunning])
+  const {
+    textBoxRef, 
+    handleChange, 
+    text, 
+    isTimeRunning, 
+    timeRemaining, 
+    startGame, 
+    wordCount
+} = useGame(5)
 
   return (
     <>
